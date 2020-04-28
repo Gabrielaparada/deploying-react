9e1fd7de6044e68864b9cb00b3ffd0d94ebdff37
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-04-28 10:56:23 -0400

    Updates

---
+++ precache-manifest.4eb5e36cf96dbdf09d278003f4e42ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0eca59300e16cfff12eafbecc8fa0f8",
+    "url": "/deploying-react/index.html"
+  },
+  {
+    "revision": "c205694bda2232d1d14f",
+    "url": "/deploying-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96cb49c4b14de86f3723",
+    "url": "/deploying-react/static/js/2.2bf54af1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploying-react/static/js/2.2bf54af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c205694bda2232d1d14f",
+    "url": "/deploying-react/static/js/main.6860f434.chunk.js"
+  },
+  {
+    "revision": "dae0acebf89b46205721",
+    "url": "/deploying-react/static/js/runtime-main.b8eb0d23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploying-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
